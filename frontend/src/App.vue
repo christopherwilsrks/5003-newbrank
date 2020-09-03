3f578bf1a3f47ecbc7d4b2e7d75ee3567e1ece97
Christopher Wilson <christopherwilsrks@gmail.com>
2020-09-04 00:58:56 +0800

    init project

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+html,
+body,
+#app {
+  font-family: sans-serif, Helvetica, Arial;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  line-height: 1.5;
+  font-size: 15px;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  /* margin-top: 60px; */
+}
+</style>
